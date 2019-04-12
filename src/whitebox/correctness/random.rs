63fb38205ea2663fda90c93612becb9c2f018962
KaoImin <kaoimin@qq.com>
2019-04-12 21:26:36 +0800

    cargo clippy

--- src/whitebox/correctness/random.rs
+++ src/whitebox/correctness/random.rs
@@ -6,9 +6,9 @@ pub(crate) fn rand_attribute(attri: u8, base: u8) -> BftTestUnit {
     let index_1: usize = rng.gen_range(0, 3);
     let index_2: usize = rng.gen_range(3, 6);
     let mut unit = [base; 6];
-    for i in 0..6 {
-        if i == index_1 || i == index_2 {
-            unit[i] = attri;
+    for (index, item) in unit.iter_mut().enumerate() {
+        if index == index_1 || index == index_2 {
+            *item = attri;
         }
     }
     unit
@@ -19,9 +19,9 @@ pub(crate) fn rand_two_attribute(attri: u8, base: u8) -> BftTestUnit {
     let index_1: usize = rng.gen_range(0, 3);
     let index_2: usize = rng.gen_range(3, 6);
     let mut unit = [base; 6];
-    for i in 0..6 {
-        if i != index_1 || i != index_2 {
-            unit[i] = attri;
+    for (index, item) in unit.iter_mut().enumerate() {
+        if index != index_1 || index != index_2 {
+            *item = attri;
         }
     }
     unit
